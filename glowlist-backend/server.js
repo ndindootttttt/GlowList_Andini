4e8e567540a57dd1155b58038d074d094df5e9c9
ndindootttttt <dia.ayu2976@smk.belajar.id>
2026-08-04 08:11:46 +0700

    download mysql2 dan menambahkan code perintah pengambilan data dari backend kategori dan produk
    3

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database GlowList');
+    }
+});
 
 app.get('/', (req, res) => {
   res.send('Selamat Datang di GlowList API 💄');
