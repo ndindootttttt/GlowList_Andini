1282ac43953d95a7f1922cbfe180632ec5c8cd13
ndindootttttt <dia.ayu2976@smk.belajar.id>
2026-08-06 12:10:43 +0700

    update produk.jsx tambah tombol

--- glowlist-frontend/src/pages/Produk.jsx
+++ glowlist-frontend/src/pages/Produk.jsx
@@ -21,6 +21,31 @@ export default function Produk() {
     getProduk();
   }, []);
 
+  const handleDelete = async (id) => {
+  if (window.confirm("Yakin ingin menghapus produk ini?")) {
+    try {
+      const res = await fetch(`http://localhost:3001/produk/${id}`, {
+        method: "DELETE",
+      });
+
+      if (res.ok) {
+        alert("Produk berhasil dihapus");
+        getProduk(); // ambil ulang data terbaru
+      } else {
+        alert("Gagal menghapus produk");
+      }
+    } catch (err) {
+      console.error("Error saat delete:", err);
+      alert("Terjadi kesalahan saat menghapus data");
+    }
+  }
+};
+
+const handleEdit = (id) => {
+  navigate(`/produk/edit/${id}`);
+};
+
+
   if (loading) {
     return (
       <div className="container mt-4">
@@ -56,6 +81,22 @@ export default function Produk() {
                 <td>{item.judul}</td>
                 <td>{item.deskripsi}</td>
                 <td>Rp {item.harga}</td>
+
+                 <td>
+          <button
+            className="btn btn-warning btn-sm me-2"
+            onClick={() => handleEdit(item.id_produk)}
+          >
+            Edit
+          </button>
+
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => handleDelete(item.id_produk)}
+          >
+            Delete
+          </button>
+        </td>
               </tr>
             ))
           ) : (
