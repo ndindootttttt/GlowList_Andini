95534502e3f5a93d57eceaea9efc4522fc95fe27
ndindootttttt <dia.ayu2976@smk.belajar.id>
2026-08-06 13:47:12 +0700

    update app.pengguna

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -121,6 +121,31 @@ app.get('/kategori', (req, res) => {
     });
 });
 
+app.post('/pengguna', async (req, res) => {
+    const { nama, email, password, no_hp } = req.body;
+
+    if (!nama || !email || !password) {
+        return res.status(400).json({ message: 'Nama, email, dan password wajib diisi' });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const sql = 'INSERT INTO pengguna (nama, email, password, no_hp) VALUES (?, ?, ?, ?)';
+
+        db.query(sql, [nama, email, hashedPassword, no_hp], (err, result) => {
+            if (err) return res.status(500).json({ error: err.sqlMessage });
+
+            res.json({
+                message: 'Akun berhasil dibuat!',
+                id_pengguna: result.insertId
+            });
+        });
+    } catch (err) {
+        res.status(500).json({ error: 'Gagal mengenkripsi password' });
+    }
+});
+
 app.listen(PORT, () => {
   console.log(`Server GlowList jalan di http://localhost:${PORT}`);
 });
