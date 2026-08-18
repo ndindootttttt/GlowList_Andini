d8644adbb787e459970099707df94495a86c946f
ndindootttttt <dia.ayu2976@smk.belajar.id>
2026-08-18 10:07:53 +0700

    update header.jsx

--- glowlist-frontend/src/components/Header.jsx
+++ glowlist-frontend/src/components/Header.jsx
@@ -1,6 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, UseNavigate } from "react-router-dom";
 
 export default function Header() {
+
+     const navigate = useNavigate();
+
+     const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/login");
+    };
     return (
         <nav className="navbar navbar-dark bg-dark px-3">
             <Link to="/" className="navbar-brand">✨ GlowList</Link>
