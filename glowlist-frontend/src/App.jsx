4806114142edd15521d9ab18f026eb9ac2b0dbc3
ndindootttttt <dia.ayu2976@smk.belajar.id>
2026-08-18 10:01:06 +0700

    update app.jsx

--- glowlist-frontend/src/App.jsx
+++ glowlist-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Produk from "./pages/Produk";
@@ -6,19 +6,40 @@ import Kategori from "./pages/Kategori";
 import Tentang from "./pages/Tentang";
 import AddProduk from "./pages/AddProduk";
 import EditProduk from "./pages/EditProduk";
+import Login from "./pages/Login";
+
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path="produk" element={<Produk />} />
-      <Route path="kategori" element={<Kategori />} />
-      <Route path="/tentang" element={<Tentang />} />
-      <Route path="produk/tambah" element={<AddProduk />} />
-      <Route path="produk/edit/:id" element={<EditProduk />} />
-    </Route>
-    </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+         <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Home />} />
+          <Route path="produk" element={<Produk />} />
+          <Route path="kategori" element={<Kategori />} />
+          <Route path="tentang" element={<Tentang />} />
+          <Route path="produk/tambah" element={<AddProduk />} />
+          <Route path="produk/edit/:id" element={<EditProduk />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- glowlist-frontend/src/pages/Login.jsx
+++ glowlist-frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/login", {
+      const res = await fetch("http://localhost:3001/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
