572fdb9f91b821703bf1935871dc0171e90a7212
ndindootttttt <dia.ayu2976@smk.belajar.id>
2026-08-18 08:19:49 +0700

    i jsonwebtoken & app.login

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
@@ -156,6 +157,52 @@ app.post('/pengguna', async (req, res) => {
     } catch (err) {
         res.status(500).json({ error: 'Gagal mengenkripsi password' });
     }
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  const sql = 'SELECT * FROM pengguna WHERE email = ?';
+
+  db.query(sql, [email], (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.sqlMessage
+      });
+    }
+
+    if (result.length === 0) {
+      return res.status(404).json({
+        message: 'Akun tidak ditemukan'
+      });
+    }
+
+    const user = result[0];
+
+    const passwordIsValid = bcrypt.compareSync(
+      password,
+      user.password
+    );
+
+    if (!passwordIsValid) {
+      return res.status(401).json({
+        message: 'Password salah'
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user.id_pengguna },
+      'glowlistrahasia',
+      { expiresIn: 86400 }
+    );
+
+    res.status(200).json({
+      auth: true,
+      token,
+      id_pengguna: user.id_pengguna,
+      nama: user.nama
+    });
+  });
 });
 
 app.listen(PORT, () => {
